030bc20a5201d11ef45eff95ab51b1a7dd39acfc
Kuba Majer <jexelem@gmail.com>
2025-05-07 18:20:24 +0200

    Dodano test klasy Pacjent, do pozniejszego przeniesienia

--- hospital_managment_app/src/hospital_managment_app/Hospital_managment_app.java
+++ hospital_managment_app/src/hospital_managment_app/Hospital_managment_app.java
@@ -3,6 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package hospital_managment_app;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -15,5 +18,16 @@ public class Hospital_managment_app {
      */
     public static void main(String[] args) {
         System.out.println("Hospital managment");
+
+        LocalDate data = LocalDate.now();  // Bieżąca data
+
+
+        List<Para<String, Integer>> recepta = new ArrayList<>();
+
+        recepta.add(new Para<>("Paracetamol", 500));
+
+        Pacjent pacjentTestowy = new Pacjent("Jan", "Nowak", "0013762830", "Brak", "0 Rh-", "Phenylephrini hydrochloridum", recepta, "Standardowe", "666666666", data, "431234647", "lalala@gmail.com", "Liliowa 1");
+        pacjentTestowy.wyswietlProfil();
+
     }
 }
