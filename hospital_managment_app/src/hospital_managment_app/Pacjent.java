3eaf0ca74b1969dce9bf22d2a91f35bc7c9d9885
Kuba Majer <jexelem@gmail.com>
2025-05-07 18:18:44 +0200

    Dodano metode wyswietlania profilu

--- hospital_managment_app/src/hospital_managment_app/Pacjent.java
+++ hospital_managment_app/src/hospital_managment_app/Pacjent.java
@@ -1,4 +1,56 @@
 package hospital_managment_app;
 
-public class Pacjent {
+import java.util.*;
+import java.time.LocalDate;
+
+public class Pacjent extends Czlowiek {
+
+    protected String historiaChoroby;
+    protected String grupaKrwi;
+    protected List<String> alergie;
+    protected List<Para<String, Integer>> perskrypcje; // w mg
+    protected String zywienie;
+    protected String numerKontaktowyBliskich;
+    protected List<Para<String, Float>> rachunek;
+    private boolean profilPacjentaKompletny = false;
+
+    // Konstruktor do zapisania pacjenta do oddzialu
+    public Pacjent(String imie, String nazwisko, String pesel,
+                   String historiaChoroby, String grupaKrwi,
+                   String alergie, List<Para<String, Integer>> perskrypcje,
+                   String zywienie, String numerKontaktowyBliskich,
+                   LocalDate dataUrodzenia,
+                   String numerTelefonu, String adresEmail, String adresZamieszkania) {
+        super(imie, nazwisko, pesel, dataUrodzenia, numerTelefonu, adresEmail, adresZamieszkania);
+
+        this.historiaChoroby = historiaChoroby;
+        this.grupaKrwi = grupaKrwi;
+        this.perskrypcje = perskrypcje;
+        this.zywienie = zywienie;
+        this.numerKontaktowyBliskich = numerKontaktowyBliskich;
+        this.alergie = new ArrayList<>();
+        this.alergie.add(alergie);  // Dodajemy alergię do listy
+        this.profilPacjentaKompletny = true;
+
+    }
+
+    // Konstruktor do zapisania pacjenta na poczekalnie
+    public Pacjent(String imie, String nazwisko, String pesel){
+        super(imie, nazwisko, pesel);
+    }
+
+
+    @Override
+    public void wyswietlProfil() {
+        System.out.println("Profil pacjenta: ");
+        super.wyswietlPodstawoweInfo();
+        if (profilPacjentaKompletny) {
+            System.out.println("Grupa krwi: " + grupaKrwi);
+            System.out.println("Alergie: " + alergie);
+            System.out.println("Perskrypcje: " + perskrypcje);
+            System.out.println("Zywienie: " + zywienie);
+            System.out.println("Numer kontaktowy bliskich: " + numerKontaktowyBliskich);
+        }
+    }
+
 }
