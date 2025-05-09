b8ab1ae1240a7e381e75ac3a3ea0740a866299e0
fidok15 <175498067+fidok15@users.noreply.github.com>
2025-05-09 18:49:24 +0200

    Dodanie abstrakcji Pomieszczenie bez raportu

--- hospital_managment_app/src/hospital_managment_app/Pomieszczenie.java
+++ hospital_managment_app/src/hospital_managment_app/Pomieszczenie.java
@@ -1,4 +1,41 @@
 package hospital_managment_app;
 
 abstract class Pomieszczenie {
+    protected int numer;
+    protected int pietro;
+    protected int pojemnoscSali;
+    protected int aktualnaPojemnosc;
+
+    public Pomieszczenie() {throw new UnsupportedOperationException("Brak danych")};
+
+    public Pomieszczenie(int numer, int pietro, int pojemnoscSali){
+        this.numer = numer;
+        this.pietro = pietro;
+        this.pojemnoscSali = pojemnoscSali;
+        this.aktualnaPojemnosc = 0;
+    }
+
+    public void dodajDoSali() {
+        if (aktualnaPojemnosc < pojemnoscSali) {
+            aktualnaPojemnosc++;
+        } else {
+            System.out.println("Sala jest już pełna!");
+        }
+    }
+
+    public boolean czyWolne() {
+        return aktualnaPojemnosc < pojemnoscSali;
+    }
+
+    public boolean czyWolne(int iluPacjentow) {
+        return (aktualnaPojemnosc + iluPacjentow) <= pojemnoscSali;
+    }
+
+    public void usunZSali() {
+        if (aktualnaPojemnosc > 0) {
+            aktualnaPojemnosc--;
+        } else {
+            System.out.println("Sala jest już pusta!");
+        }
+    }
 }
