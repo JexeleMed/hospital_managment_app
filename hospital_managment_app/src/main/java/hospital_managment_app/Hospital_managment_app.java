bdfc564dc0325bffcf01ed1092a102d7f880d05c
Kuba Majer <jexelem@gmail.com>
2025-05-21 17:23:56 +0200

    Test do zapisu

--- hospital_managment_app/src/main/java/hospital_managment_app/Hospital_managment_app.java
+++ hospital_managment_app/src/main/java/hospital_managment_app/Hospital_managment_app.java
@@ -17,6 +17,25 @@ public class Hospital_managment_app {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        List<Para<String, Integer>> perskrypcje = new ArrayList<>();
+        perskrypcje.add(new Para<>("Lek A", 500)); // przykładowa preskrypcja
+        Pacjent pacjent = new Pacjent("Jan", "Kowalski", "12345678901",
+                "A+", "Orzechy", perskrypcje,
+                "wegetariańska", "987654321",
+                LocalDate.of(1985, 5, 15),
+                "123456789", "jan.kowalski@email.com",
+                "ul. Przykładowa 1");
 
+
+        List<Pacjent> pacjents = new ArrayList<>();
+        pacjents.add(pacjent);
+
+        PacjentHandler pacjentHandler = new PacjentHandler();
+
+        pacjentHandler.saveAll(pacjents);
     }
+
+
+
 }
+
