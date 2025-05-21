0da6817dbcfb74ee60ff484aa7d732404a24c980
Kuba Majer <jexelem@gmail.com>
2025-05-21 16:26:07 +0200

    Konstrukcja handler

--- hospital_managment_app/src/main/java/hospital_managment_app/HandlerCsv.java
+++ hospital_managment_app/src/main/java/hospital_managment_app/HandlerCsv.java
@@ -1,6 +1,11 @@
 package hospital_managment_app;
 import java.io.IOException;
 import java.util.*;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 public interface HandlerCsv<T> {
     List<T> loadAll() throws IOException;
---
+++ hospital_managment_app/src/main/java/hospital_managment_app/PacjentHandler.java
@@ -0,0 +1,7 @@
+package hospital_managment_app;
+
+import java.util.logging.Handler;
+
+public class PacjentHandler implements HandlerCsv<Pacjent> {
+
+}
