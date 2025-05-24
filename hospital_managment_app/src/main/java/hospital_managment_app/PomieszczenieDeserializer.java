c2e2356fc6fa5a1d76afb95c8c31b25ff038a800
fidok15 <175498067+fidok15@users.noreply.github.com>
2025-05-24 20:52:35 +0200

    Naprawa PomiesczenieDeseralizer

--- hospital_managment_app/src/main/java/hospital_managment_app/PomieszczenieDeserializer.java
+++ hospital_managment_app/src/main/java/hospital_managment_app/PomieszczenieDeserializer.java
@@ -1,5 +1,12 @@
 package hospital_managment_app;
 
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonParseException;
+import java.lang.reflect.Type;
+
 public class PomieszczenieDeserializer implements JsonDeserializer<Pomieszczenie> {
     @Override
     public Pomieszczenie deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
--- hospital_managment_app/src/main/java/hospital_managment_app/PomieszczenieSerializer.java
+++ hospital_managment_app/src/main/java/hospital_managment_app/PomieszczenieSerializer.java
@@ -1,5 +1,11 @@
 package hospital_managment_app;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializer;
+import com.google.gson.JsonSerializationContext;
+import java.lang.reflect.Type;
+
 public class PomieszczenieSerializer implements JsonSerializer<Pomieszczenie> {
     @Override
     public JsonElement serialize(Pomieszczenie src, Type typeOfSrc, JsonSerializationContext context) {
